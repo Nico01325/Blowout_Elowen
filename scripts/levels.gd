6ebb2f3fa77017ece630f4cd1215fd85bbab8c79
nico01325 <marcolacaradebola230@gmail.com>
2025-11-05 15:36:44 -0300

    versåo concertada

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -1,22 +1,24 @@
 extends Node2D
 
-@onready var player = $"."
-
+@export var player : Node2D  # arraste o Player da cena para esse campo no editor
 
 func _ready() -> void:
 	if Global.destination_level != "":
 		var point = get_node(Global.destination_level)
 		if point: 
-			Player.global_position = point.global_position
-			
-
+			player.global_position = point.global_position
 
 func _on_spawner_de_inimigo_timeout() -> void:
 	var cena_inimigo = preload("res://cenas/Inimigo.tscn") 
 	var novo_inimigo = cena_inimigo.instantiate()
-	novo_inimigo.Player = $Player
+	
+	# Passa o jogador atual para o inimigo
+	novo_inimigo.player = player
+
+	# Gera posição aleatória em volta do jogador
 	var random = RandomNumberGenerator.new()
-	var posicaoJogador = $Player.global_position
-	novo_inimigo.position = Vector2(posicaoJogador.x + random.randi_range(100,200), posicaoJogador.y + random.randi_range(100,200) )
+	var posicaoJogador = player.global_position
+	var offset = Vector2(random.randi_range(100, 200), random.randi_range(100, 200))
+	
+	novo_inimigo.position = posicaoJogador + offset
 	add_child(novo_inimigo)
-	pass # Replace with function body.
