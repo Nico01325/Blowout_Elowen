f7f7bc0cd04642909160f250ef8c0e607d1bc38a
Nicolas <marcolacaradebola230@gmail.com>
2025-11-05 14:54:55 -0300

    implementaçåo de alguns ajustes nas fases
    inimigo fora da fase 1

--- scripts/inimigo.gd
+++ scripts/inimigo.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
 
-@onready var navigation_agent_2d = $NavigationAgent2D
-@onready var detection_area = $DetectionArea
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var detection_area: Area2D = $DetectionArea
 
-@export var Player: Player
-@export var Speed: float = 100
+@export var player: CharacterBody2D
+@export var speed: float = 100.0
 
 var perseguindo := false
 
 func _physics_process(delta):
 	if perseguindo:
-		var direction = Vector2(Player.global_position.x-self.global_position.x, Player.global_position.y-self.global_position.y).normalized()
-		velocity = direction * Speed
+		var next_path_position = navigation_agent_2d.get_next_path_position()
+		var direction = (next_path_position - global_position).normalized()
+		velocity = direction * speed
 	else:
 		velocity = Vector2.ZERO
 
@@ -19,20 +20,23 @@ func _physics_process(delta):
 
 func _on_timer_timeout():
 	if perseguindo:
-		navigation_agent_2d.target_position = Player.global_position
-
+		navigation_agent_2d.target_position = player.global_position
 
 func _on_DetectionArea_body_entered(body):
-	if body == Player:
+	if body == player:
 		perseguindo = true
-		navigation_agent_2d.target_position = Player.global_position
+		navigation_agent_2d.target_position = player.global_position
 
 func _on_DetectionArea_body_exited(body):
-	if body == Player:
+	if body == player:
 		perseguindo = false
-		
-		
+
+# Quando o inimigo encosta no jogador
 func _on_Area2D_body_entered(body):
-		if body.name == "Player":
-			get_tree().reload_current_scene()
-# Fiz comendo pão
+	if body == player:
+		matar()
+
+# Função genérica para "matar" o jogador
+func matar():
+	print("O jogador foi pego!")
+	get_tree().reload_current_scene()
